064e7f36870942a14e6d0298996c2a9516e6f0d5
Hanna Boratynska <boratynskahanna@gmail.com>
2021-05-06 20:40:54 +0200

    Add card option with icon

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -4,9 +4,14 @@ import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
 import Creator from '../Creator/Creator.js';
 import { settings } from '../../data/dataStore';
+import Icon from '../Icon/Icon.js';
 
 
 class Column extends React.Component {
+    state = {
+    cards: this.props.cards || [],
+}
+
     static propTypes = {
         title: PropTypes.string.isRequired,
         cards: PropTypes.array,
@@ -15,7 +20,7 @@ class Column extends React.Component {
   render() {
     return (
       <section className={styles.component}>
-        <h3 className={styles.title}> {this.props.title} </h3>
+        <h3 className={styles.title}> {this.props.title} <span className={styles.icon}> <Icon name={this.props.icon} /> </span></h3>
 
         <div className={styles.cards}>
           {this.state.cards.map(({key, ...cardProps}) => (
@@ -30,6 +35,20 @@ class Column extends React.Component {
       </section>
     )
   }
+
+  addCard(title){
+    this.setState(state => (
+      {
+        cards: [
+          ...state.cards,
+          {
+            key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
+            title,
+          }
+        ]
+      }
+    ));
+}
 
 }
 
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,8 +4,8 @@ import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
 import Column from '../Column/Column.js';
 import {settings} from '../../data/dataStore';
-import ReactHtmlParser from 'react-html-parser';
 import Creator from '../Creator/Creator.js';
+import ReactHtmlParser from 'react-html-parser';
 
 
 class List extends React.Component {
