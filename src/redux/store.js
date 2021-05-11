6764bc23c9eca6531986c1e0a7769ff649d59a15
Hanna Boratynska <boratynskahanna@gmail.com>
2021-05-11 20:40:12 +0200

    Fix redux folders

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import {combineReducers, createStore} from 'redux';
-import initialStoreData from '../src/data/dataStore';
+import initialStoreData from '../data/dataStore';
 
 // define initial state and shallow-merge initial data
 const initialState = {
